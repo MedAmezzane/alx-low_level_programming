0998ae1014edf6b99f8a0a536b09fe3f0d28ec9a
Mohamed AMEZZANE <Med.AMEZZANE@gmail.com>
2023-07-14 15:25:18 +0100

    'I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game' task

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase, followed by
+ * a new line.
+ *
+ * Description:
+ * Use `putchar` function to prints the alphabet in lowercase,
+ * followed by a new line.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char alpha[26] = "abcdefghijklmnopqrstuvwxyz";
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alpha[i]);
+	}
+	putchar('\n');
+	return (0);
+}
