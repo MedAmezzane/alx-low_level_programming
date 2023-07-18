731eb58b5f472f2bcce0120229d4d43310bd92c4
Mohamed AMEZZANE <Med.AMEZZANE@gmail.com>
2023-07-18 23:05:54 +0100

    'The World looks like a multiplication-table, or a mathematical equation, which, turn it how you will, balances itself' task

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -23,35 +23,36 @@ void print_times_table(int n)
 	{
 		for (num1 = 0; num1 < n + 1; num1++)
 		{
-			_putchar('0');
 			for (num2 = 0; num2 < n + 1; num2++)
 			{
 				prod = num1 * num2;
-				_putchar(',');
-				_putchar(' ');
-				if (prod < 10 && num2 != 0)
+				if (num2 != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+				if (prod >= 100 && num2 != 0)
+				{
+					/*get the three digits*/
+					_putchar((prod / 100) + '0');
+					_putchar(((prod / 10)) % 10 + '0');
+					_putchar(prod % 10 + '0');
+				}
+				else if (prod >= 10 && prod < 100)
+				{
+					_putchar(' ');
+					/*get the two digits*/
+					_putchar((prod / 10) + '0');
+					_putchar((prod % 10) + '0');
+				}
+				else if (prod < 10 && (num2 != 0))
 				{
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(prod + '0');
 				}
-				else if (prod >= 10 && prod <= 99)
-				{
-					_putchar(' ');
-					/*get the first digit*/
-					_putchar((prod / 10) + '0');
-					/*get the second digit*/
-					_putchar((prod % 10) + '0');
-				}
-				else if (prod >= 100)
-				{
-					/*get the first digit*/
-					_putchar((prod / 100) + '0');
-					/*get the second digit*/
-					_putchar(((prod / 10)) % 10 + '0');
-					/*get the third digit*/
+				else
 					_putchar(prod % 10 + '0');
-				}
 			}
 			_putchar('\n');
 		}
