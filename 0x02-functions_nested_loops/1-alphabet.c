809a43bb58e204c8e0fedd17c9c2ba7955d44263
Mohamed AMEZZANE <Med.AMEZZANE@gmail.com>
2023-07-18 16:58:12 +0100

    'I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game' task

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description:
+ * Use the custom `_putchar` function to prints the alphabets in lowercase,
+ * followed by a new line
+ *
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	char alpha[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		_putchar(alpha[i]);
+	}
+	_putchar('\n');
+}
