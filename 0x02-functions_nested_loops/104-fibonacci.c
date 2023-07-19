9ed39e327ad42d0bc0911fac8dd7aec53e34074b
Mohamed AMEZZANE <Med.AMEZZANE@gmail.com>
2023-07-19 01:47:53 +0100

    'In computer class, the first assignment was to write a program to print the first 100 Fibonacci numbers. Instead, I wrote a program that would steal passwords of students. My teacher gave me an A+' task

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -5,7 +5,7 @@
  *
  * Description:
  * Prints the first 98 Fibonacci numbers, starting with 1
- * and 2, followed by a new line.
+ * and 2, without using long long, followed by a new line.
  *
  * ps: The numbers are separated by comma, followed by a space.
  *
@@ -15,16 +15,36 @@ int main(void)
 {
 	int i;
 	unsigned long f0 = 1, f1 = 2, sum;
+	unsigned long f0_half1, f0_half2, f1_half1, f1_half2;
+	unsigned long sum_half1, sum_half2;
 
 	printf("%lu, %lu", f0, f1);
-	for (i = 2; i < 98; i++)
+	for (i = 2; i < 92; i++)
 	{
 		sum = f1 + f0;
 		printf(", %lu", sum);
 		f0 = f1;
 		f1 = sum;
 	}
-	printf("\n");
+	f0_half1 = f0 / 10000000000;
+	f0_half2 = f0 % 10000000000;
+	f1_half1 = f1 / 10000000000;
+	f1_half2 = f1 % 10000000000;
+	for (i = 92; i < 98; i++)
+	{
+		sum_half1 = f0_half1 + f1_half1;
+		sum_half2 = f0_half2 + f1_half2;
+		if (f0_half2 + f1_half2 > 9999999999)
+		{
+			sum_half1 += 1;
+			sum_half2 %= 10000000000;
+		}
+		printf(", %lu%lu", sum_half1, sum_half2);
+		f0_half1 = f1_half1;
+		f0_half2 = f1_half2;
+		f1_half1 = sum_half1;
+		f1_half2 = sum_half2;
+	}
 
 	return (0);
 }
