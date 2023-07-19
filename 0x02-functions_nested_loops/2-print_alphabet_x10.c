ebbcf3e7bff00cb63babd864850458cb52495d5c
Mohamed AMEZZANE <Med.AMEZZANE@gmail.com>
2023-07-19 02:33:05 +0100

    '10 x alphabet' task

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,23 +3,17 @@
  * print_alphabet_x10 - Entry point
  *
  * Description:
- * Use the custom `_putchar` function to prints the alphabets in lowercase,
- * followed by a new line, 10 times.
+ * Use the custom `print_alphabet` function to prints the alphabets in
+ * lowercase, followed by a new line, 10 times.
  *
  * Return: 0
  */
-
 void print_alphabet_x10(void)
 {
-	char alpha[26] = "abcdefghijklmnopqrstuvwxyz";
-	int i, j;
+	int j;
 
 	for (j = 0; j < 10; j++)
 	{
-		for (i = 0; i < 26; i++)
-		{
-			_putchar(alpha[i]);
-		}
-	_putchar('\n');
+	print_alphabet();
 	}
 }
