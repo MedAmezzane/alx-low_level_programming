b86be46ca77c80c264ccc4a87d56e055b31d5a1f
Mohamed AMEZZANE <Med.AMEZZANE@gmail.com>
2023-08-02 03:04:17 +0100

    'strpbrk' task

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -19,19 +19,21 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-	int i, j;
+	unsigned int i, j;
 
-	for (i = 0; *s != '\0'; i++)
+	for (i = 0; s[i]; i++)
 	{
-		for (j = 0; accept[j] != '\0'; j++)
+		for (j = 0; accept[j]; j++)
 		{
-			if (*s == accept[j])
+			if (s[i] == accept[j])
 			{
-				return (s);
+				break;
 			}
 		}
-		s++;
+		if (accept[j] != '\0')
+		{
+			return (&s[i]);
+		}
 	}
-
-	return (NULL);
+	return (0);
 }
