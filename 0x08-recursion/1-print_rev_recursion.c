2d9b10f88108b1d8dc2a1877486552b9172605bd
Mohamed AMEZZANE <Med.AMEZZANE@gmail.com>
2023-08-03 01:15:25 +0100

    'Why is it so important to dream? Because, in my dreams we are together' task

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _puts_recursion - Entry point
+ * @s: String to print in reverse order using recursion.
+ *
+ * Description:
+ * A a recursive implementation of a function that prints a
+ * string in reverse order.
+ *
+ * Return: No return.
+ *
+ * Author: MedAMEZZANE
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
