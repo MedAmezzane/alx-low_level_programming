cccc93593bfa9777a6421d71a71d30463c9d7136
Mohamed AMEZZANE <Med.AMEZZANE@gmail.com>
2023-08-09 01:36:39 +0100

    'Float like a butterfly, sting like a bee' task

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * create_array - Entry point
+ * @size: The size of the array to create.
+ * @c: The character to initialize each element of the array with.
+ *
+ * Description: Allocates memory for an array of characters and initializes
+ *              each element with the given character.
+ * Returns: A pointer to the newly allocated array, or NULL if memory
+ * allocation fails or if size is 0.
+ *
+ * Author: MedAMEZZANE
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *ptr;           /* Pointer to the allocated memory block */
+	unsigned int i = 0;  /* Loop counter */
+
+	if (size == 0)
+		return (NULL);   /* Return NULL if size is 0 */
+
+	ptr = malloc(size * sizeof(char)); /* Allocate memory for the array */
+	if (ptr == NULL)
+		return (NULL);   /* Return NULL if memory allocation fails */
+
+	while (i < size)
+	{
+		ptr[i] = c;      /* Initialize each element with the given character */
+		i++;
+	}
+
+	return (ptr);        /* Return the pointer to the allocated array */
+}
