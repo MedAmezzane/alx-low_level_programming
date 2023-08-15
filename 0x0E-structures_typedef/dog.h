2835d5509e7db7207e312eab391b73cdd429c65c
Mohamed AMEZZANE <Med.AMEZZANE@gmail.com>
2023-08-15 03:36:56 +0100

    'A dog will teach you unconditional love. If you can have that in your life, things won't be too bad' task

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,54 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - Represents a dog's information.
+ * @name: Name of the dog.
+ * @age: Age of the dog.
+ * @owner: Owner's name of the dog.
+ *
+ * Description: This structure defines the attributes of a dog.
+ */
+struct dog
+{
+	tab char *name;
+	tab float age;
+	tab char *owner;
+};
+
+/**
+ * dog_t - Typedef for struct dog.
+ */
+typedef struct dog dog_t;
+
+/**
+ * init_dog - Initializes a dog structure.
+ * @d: Pointer to the dog structure to be initialized.
+ * @name: Name of the dog.
+ * @age: Age of the dog.
+ * @owner: Owner's name of the dog.
+ *
+ * Description: This function initializes a dog structure with given attributes.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+/**
+ * print_dog - Prints the information of a dog.
+ * @d: Pointer to the dog structure to be printed.
+ *
+ * Description: This function prints the attributes of a dog structure.
+ */
+void print_dog(struct dog *d);
+
+/**
+ * new_dog - Creates a new dog structure.
+ * @name: Name of the dog.
+ * @age: Age of the dog.
+ * @owner: Owner's name of the dog.
+ *
+ * Returns: Pointer to the newly created dog structure.
+ */
+dog_t *new_dog(char *name, float age, char *owner);
+
+
+#endif /* _DOG_H_ */
