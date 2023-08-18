fcb59d209cc477d8f4da4a11e870d6e1d2e75f23
Mohamed AMEZZANE <Med.AMEZZANE@gmail.com>
2023-08-18 01:29:01 +0100

    'Beauty is variable, ugliness is constant' task

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -4,25 +4,25 @@
 
 /**
  * sum_them_all - Calculates the sum of a variable number of integers.
- * @num_count: Number of integers to be summed.
+ * @n: Number of integers to be summed.
  * @...: Variable list of integers to be summed.
  *
  * Return: The sum of the given integers.
  *
  * Author: MedAMEZZANE
  */
-int sum_them_all(const unsigned int num_count, ...)
+int sum_them_all(const unsigned int n, ...)
 {
 	va_list args;
 	int sum = 0;
 	unsigned int i;
 
-	if (num_count == 0)
+	if (n == 0)
 		return (0);
 
-	va_start(args, num_count);
+	va_start(args, n);
 
-	for (i = 0; i < num_count; i++)
+	for (i = 0; i < n; i++)
 	{
 		sum += va_arg(args, int);
 	}
