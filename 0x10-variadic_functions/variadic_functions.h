f2e03c60c5ddde7f3c53b30134441b7899308ab9
Mohamed AMEZZANE <Med.AMEZZANE@gmail.com>
2023-08-18 02:23:52 +0100

    'To be is a to be the value of a variable' task

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -8,5 +8,16 @@ void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 int _putchar(char c);
 
+/**
+ * struct print - A structure representing a format specifier and its
+ * corresponding printing function.
+ * @t: The format specifier string.
+ * @f: Pointer to the printing function that handles the format specifier.
+ */
+typedef struct print
+{
+	char *t;
+	void (*f)(va_list);
+} print_t;
 
 #endif
