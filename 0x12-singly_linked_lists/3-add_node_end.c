f73b86155ef018106092c4874ba922f082cffb49
Mohamed AMEZZANE <Med.AMEZZANE@gmail.com>
2023-08-26 00:15:52 +0100

    'Add node at the end' task

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,28 +1,33 @@
 #include "lists.h"
-
 /**
- * add_node - Adds a new node at the beginning of a linked list.
+ * add_node_end - Adds a new node at the end of a linked list.
  * @head: A pointer to a pointer to the head of the linked list.
  * @str: The string to be stored in the new node.
  * Return: A pointer to the newly added node, or NULL on failure.
  *
- * Description: This function creates a new node and adds it to the beginning
- * of a linked list. The new node is populated with a copy of the provided
- * string and its length. The function updates the head pointer and returns
- * a pointer to the newly added node. Returns NULL on memory allocation failure
+ * Description: This function creates a new node and adds it to the end of a
+ * linked list. The new node is populated with a copy of the provided string
+ * and its length. The function updates the head pointer if necessary and
+ * returns a pointer to the newly added node. Returns NULL on memory allocation
+ * failure.
+ *
+ * Author: MedAMEZZANE
  */
-list_t *add_node(list_t **head, const char *str)
+list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new_node;
+	list_t *temp = *head;
 	unsigned int str_len = 0;
 
+	/*Calculate the length of the input string*/
 	while (str[str_len])
 		str_len++;
-
+	/*Allocate memory for the new node*/
 	new_node = malloc(sizeof(list_t));
 	if (!new_node)
+		/* Return NULL on memory allocation failure*/
 		return (NULL);
-
+	/*Allocate memory and copy the input string to the new node*/
 	new_node->str = strdup(str);
 	if (!new_node->str)
 	{
@@ -31,8 +36,19 @@ list_t *add_node(list_t **head, const char *str)
 	}
 
 	new_node->len = str_len;
-	new_node->next = (*head);
-	(*head) = new_node;
+	new_node->next = NULL;
 
-	return (*head);
+	if (*head == NULL)
+	{
+		/*If list is empty, make new node the head*/
+		*head = new_node;
+		return (new_node);
+	}
+	/*Traverse to the last node and update its next pointer*/
+	while (temp->next)
+		temp = temp->next;
+
+	temp->next = new_node;
+
+	return (new_node);
 }
