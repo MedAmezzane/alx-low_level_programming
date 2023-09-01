2801f95d84f719e3d444024ef36a3a11b5b07988
Mohamed AMEZZANE <Med.AMEZZANE@gmail.com>
2023-09-01 02:34:06 +0100

    '101' task

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * flip_bits - Count the number of bits needed to flip to transform one number to another.
+ * @n: The first unsigned long int.
+ * @m: The second unsigned long int.
+ * Return: The number of bits needed to flip.
+ *
+ * Description:
+ * This function counts the number of bits that need to be flipped in order to
+ * transform one unsigned long int (n) to another (m). The XOR operation (^)
+ * is used to find the differing bits between the two numbers. Then, a loop
+ * iterates through each bit position, and the count of differing bits is accumulated.
+ * The function returns the total count of differing bits.
+ *
+ * Author: MedAMEZZANE
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int i, count = 0;
+	unsigned long int current;
+	unsigned long int differing_bits = n ^ m;
+
+	for (i = 63; i >= 0; i--)
+	{
+		current = differing_bits >> i;
+		if (current & 1)
+			count++; /*Count set bits*/
+	}
+
+	return (count); /*Return the count of differing bits*/
+}
