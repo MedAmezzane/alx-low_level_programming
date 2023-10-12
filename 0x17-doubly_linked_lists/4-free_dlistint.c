cb357f18875430e8049724e7ae7375eebb329c11
Med AMEZZANE <Med.AMEZZANE@gmail.com>
2023-10-12 23:59:19 +0100

    Task 04

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_dlistint - Frees a doubly linked list.
+ * @head: A pointer to the head of the doubly linked list.
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *current, *next_node;
+
+	while (head != NULL)
+	{
+		next_node = head->next;  /*Store the next node temporarily.*/
+		free(head);              /*Free the current node.*/
+		head = next_node;        /*Move to the next node.*/
+	}
+}
